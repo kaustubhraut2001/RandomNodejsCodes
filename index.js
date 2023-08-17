852a7d131f7042764ffa61970dd27d88f8c00acb
kaustubhr2001@gmail.com <87487138+kaustubhraut2001@users.noreply.github.com>
2023-08-17 18:12:54 +0530

    fourth

--- index.js
+++ index.js
@@ -2,51 +2,74 @@ const http = require('http');
 const dotenv = require('dotenv');
 const fs = require("fs");
 const url = require('url');
-
 dotenv.config();
+const express = require("express");
 
-const server = http.createServer((req, res) => {
-    const log = Date.now() + ' ' + req.method + ' ' + "New request reacived" + '\n';
-    const urll = url.parse(req.url, true);
-    console.log(urll.query)
+const app = express();
 
-
-    fs.appendFile('log.txt', log, (err, result) => {
-
-        switch (urll.pathname) {
-            case '/':
-                res.end('Home Page');
-                break;
-            case '/about':
-                const a = urll.query.a;
-
-                res.end(`About Page + ${a}`);
-                break;
-            case '/signup':
-                if (req.method === 'GET') {
-                    res.end('Signup Page');
-                } else if (req.method === 'POST') {
-                    res.end('Signup Page');
+app.get('/', (req, res) => {
 
 
-
-                }
-            default:
-                res.end('Something went wrong');
-
-
-        }
-        // if (err)
-        //     console.log(err);
-        // else
-        //     console.log("File Write Successfully ");
-
-    });
-
-
-    // res.end('Server Started successfully');
+    return res.send('Home Page');
 });
 
-server.listen(process.env.PORT, () => {
+
+app.get('/about', (req, res) => {
+
+
+    return res.send('About Page');
+});
+
+app.get('/signup', (req, res) => {
+    return res.send(`Signup Page ${req.query.name}`);
+
+
+});
+
+
+
+// const server = http.createServer((req, res) => {
+//     const log = Date.now() + ' ' + req.method + ' ' + "New request reacived" + '\n';
+//     const urll = url.parse(req.url, true);
+//     console.log(urll.query)
+
+
+//     fs.appendFile('log.txt', log, (err, result) => {
+
+//         switch (urll.pathname) {
+//             case '/':
+//                 res.end('Home Page');
+//                 break;
+//             case '/about':
+//                 const a = urll.query.a;
+
+//                 res.end(`About Page + ${a}`);
+//                 break;
+//             case '/signup':
+//                 if (req.method === 'GET') {
+//                     res.end('Signup Page');
+//                 } else if (req.method === 'POST') {
+//                     res.end('Signup Page');
+
+
+
+//                 }
+//             default:
+//                 res.end('Something went wrong');
+
+
+//         }
+//         // if (err)
+//         //     console.log(err);
+//         // else
+//         //     console.log("File Write Successfully ");
+
+//     });
+
+
+//     // res.end('Server Started successfully');
+// });
+
+app.listen(process.env.PORT, () => {
     console.log(`Server is running ${process.env.PORT}`);
 });
