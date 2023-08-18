cca52cc662e81fa8b5d3872c80c7d598bac67852
kaustubhr2001@gmail.com <87487138+kaustubhraut2001@users.noreply.github.com>
2023-08-18 13:32:25 +0530

    eight

--- index.js
+++ index.js
@@ -9,6 +9,43 @@ const express = require("express");
 
 const app = express();
 
+
+// Express Middleware -- we can trate it as plugin
+app.use(express.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+    fs.appendFile("log.txt", Date.now() + ' ' + req.method + ' ' + "New request reacived", (err, result) => {
+        if (err)
+            console.log(err);
+        else
+            console.log("File Write Successfully ");
+
+    });
+    console.log("Middleware");
+    next();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 app.get('/api/user', (req, res) => {
 
     return res.json(data);
